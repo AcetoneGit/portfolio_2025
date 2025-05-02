b502971b0e1bb25b01654a45e92c7a1da2998ed4
Acetone <antoine.c@etik.com>
2025-05-02 21:19:58 +0200

    optimization for speed of reveal

--- app/javascript/controllers/scroll_reveal_controller.js
+++ app/javascript/controllers/scroll_reveal_controller.js
@@ -10,52 +10,52 @@ export default class extends Controller {
     // Configuration de base
     const config = {
       distance: '50px',
-      duration: 800,
+      duration: 250,
       easing: 'cubic-bezier(0.5, 0, 0, 1)',
-      interval: 100
+      interval: 50
     };
 
     ScrollReveal().reveal('.skills-container', {
       ...config,
-      delay: 300,
+      delay: 100,
       origin: 'bottom'
     });
 
     ScrollReveal().reveal('.projects-container', {
       ...config,
-      delay: 400,
+      delay: 150,
       origin: 'bottom'
     });
 
     ScrollReveal().reveal('.education-container', {
       ...config,
-      delay: 500,
+      delay: 200,
       origin: 'bottom'
     });
 
     ScrollReveal().reveal('.codewars-container', {
       ...config,
-      delay: 600,
+      delay: 250,
       origin: 'bottom'
     });
 
     ScrollReveal().reveal('.skill-card', {
       ...config,
-      delay: 300,
+      delay: 100,
       interval: 100,
       origin: 'bottom'
     });
 
     ScrollReveal().reveal('.project-card', {
       ...config,
-      delay: 400,
+      delay: 150,
       interval: 150,
       origin: 'bottom'
     });
 
     ScrollReveal().reveal('.education-item', {
       ...config,
-      delay: 500,
+      delay: 200,
       interval: 150,
       origin: 'left'
     });
